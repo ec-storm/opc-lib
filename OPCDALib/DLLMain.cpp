f2d5316fbab28527874b758bf3c3eac6f4c82295
minhdtb <minhdtb.83@gmail.com>
2016-05-22 13:06:14 +0700

    optimize code

--- OPCDALib/DLLMain.cpp
+++ OPCDALib/DLLMain.cpp
@@ -2,18 +2,22 @@
 
 OPCClient* g_client;
 
-JNIEXPORT void JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_initialize(JNIEnv *env, jobject jobj) {
+JNI_FUNCTION(initialize, void)(JNIEnv *env, jobject jobj) {
 	g_client = new OPCClient();
 }
 
-JNIEXPORT void JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_connect(JNIEnv *env, jobject jobj, jstring host, jstring progId) {
+JNI_FUNCTION(uninitialize, void)(JNIEnv *env, jobject jobj) {
+	delete g_client;
+}
+
+JNI_FUNCTION(connect, void)(JNIEnv *env, jobject jobj, jstring host, jstring progId) {
 	if (g_client == NULL)
 		return;
 
 	g_client->Connect((char*)env->GetStringUTFChars(host, 0), (char*)env->GetStringUTFChars(progId, 0));
 }
 
-JNIEXPORT jobjectArray JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_getOpcServers(JNIEnv *env, jobject jobj, jstring host) {
+JNI_FUNCTION(getOpcServers, jobjectArray)(JNIEnv *env, jobject jobj, jstring host) {
 	const char * chost = env->GetStringUTFChars(host, NULL);
 
 	std::vector<std::string> stringList = g_client->GetOPCServers((char*)env->GetStringUTFChars(host, 0));
@@ -28,7 +32,7 @@ JNIEXPORT jobjectArray JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManage
 	return ret;
 }
 
-JNIEXPORT jobjectArray JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_getOpcServerTags(JNIEnv *env, jobject jobj) {	
+JNI_FUNCTION(getOpcServerTags, jobjectArray)(JNIEnv *env, jobject jobj) {
 
 	std::vector<std::string> stringList = g_client->GetServerTags();
 
@@ -62,5 +66,6 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 		break;
 	}
 	}
+
 	return TRUE;
 }
--- OPCDALib/DLLMain.h
+++ OPCDALib/DLLMain.h
@@ -5,14 +5,17 @@
 #include <windows.h>
 #include "OPCClient.h"
 
+#define JNI_FUNCTION(name, ret) JNIEXPORT ret JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_##name
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	JNIEXPORT void JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_initialize(JNIEnv *env, jobject jobj);
-	JNIEXPORT void JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_connect(JNIEnv *env, jobject jobj, jstring host, jstring progId);
-	JNIEXPORT jobjectArray JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_getOpcServers(JNIEnv *env, jobject jobj, jstring host);
-	JNIEXPORT jobjectArray JNICALL Java_com_minhdtb_storm_core_lib_opcda_OPCDAManager_getOpcServerTags(JNIEnv *env, jobject jobj);
+	JNI_FUNCTION(initialize, void)(JNIEnv *env, jobject jobj);
+	JNI_FUNCTION(uninitialize, void)(JNIEnv *env, jobject jobj);
+	JNI_FUNCTION(connect, void)(JNIEnv *env, jobject jobj, jstring host, jstring progId);
+	JNI_FUNCTION(getOpcServers, jobjectArray)(JNIEnv *env, jobject jobj, jstring host);
+	JNI_FUNCTION(getOpcServerTags, jobjectArray)(JNIEnv *env, jobject jobj);
 
 #ifdef __cplusplus
 }
